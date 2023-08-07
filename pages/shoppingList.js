fa680d16bf1a30a6a0e4c0bc84caa0bf59ec1e7d
Aaron Hill <aahill50@gmail.com>
2023-08-07 13:23:17 -0400

    add basic shopping list page

---
+++ pages/shoppingList.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import utils from '../utils';
+import styles from './shoppingList.module.css';
+
+export default function ShoppingList() {
+    return <div>Add items to your Shopping List</div>
+}
