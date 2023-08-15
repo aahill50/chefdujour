46b0ad801613aa3b5a1c401716224c44c007a77b
Aaron Hill <aahill50@gmail.com>
2023-08-15 18:58:17 -0400

    disable facebook, enable google oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,37 +2,34 @@
    will also contain all of the global NextAuth.js configurations. */
 
 import NextAuth from 'next-auth';
-import FacebookProvider from 'next-auth/providers/facebook';
-// import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/google';
 // import InstagramProvider from 'next-auth/providers/instagram';
 // import OktaProvider from 'next-auth/providers/okta';
+const callbackUrlDev = 'https://chefdujour-git-auth-aahill50.vercel.app//api/auth/callback/google'
+const callbackUrlProd = 'https://chefdujour.vercel.app//api/auth/callback/google'
 
 export const authOptions = {
-    // callbacks: {
-    //     async jwt({ token, account }) {
-    //         // Persist the OAuth access_token to the token right after signin
-    //         if (account) {
-    //             token.accessToken = account.access_token;
-    //         }
-    //         return token;
-    //     },
-    //     async session({ session, token, user }) {
-    //         console.log('session:', session);
-    //         // Send properties to the client, like an access_token from a provider.
-    //         session.accessToken = token.accessToken;
-    //         return session;
-    //     },
-    // },
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token, user }) {
+            console.log('session:', session);
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
     // Configure one or more authentication providers
     providers: [
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        // }),
         // InstagramProvider({
         //     clientId: process.env.INSTAGRAM_CLIENT_ID,
         //     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
@@ -45,3 +42,4 @@ export const authOptions = {
     ],
 };
 export default NextAuth(authOptions);
+
