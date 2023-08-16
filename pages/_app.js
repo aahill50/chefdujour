3f6ba07214cef2eba0825e7d1fc7a2da75622952
Aaron Hill <aahill50@users.noreply.github.com>
2023-08-16 12:42:31 -0400

    Auth (#4)
    * added next auth
    
    * wip auth
    
    * enable only facebook login
    
    * disable facebook, enable google oauth
    
    * cleanup auth code

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
+import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 
 import '../styles/global.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}) {
     return (
-        <Layout>
-            <Header />
-            <Component {...pageProps} />
-        </Layout>
+        <SessionProvider session={session}>
+            <Layout>
+                <Header />
+                <Component {...pageProps} />
+            </Layout>
+        </SessionProvider>
     );
 }
